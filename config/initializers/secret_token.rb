d7d0c3a1cb8016b8c8a2dc712fa2909f9fa98931
Kenneth Chen <kennchen@gmail.com>
2013-07-30 13:03:01 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yfas::Application.config.secret_token = 'c9615353d542c34397865a630af31c4b8ba0d0ffa55e6c0752ac74410aa320188e7bf2426c9aa061c274c268dc320c969d7c028e75315b5bf606ebfb19e07fc2'
